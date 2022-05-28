00d1c973527a81d1b2ca2c1bfc5710110e67c26c
shamardn <shamardn@gmail.com>
2022-05-29 01:30:20 +0200

    add feature of top 10 installs

--- src/main/kotlin/Analyzer.kt
+++ src/main/kotlin/Analyzer.kt
@@ -1,3 +1,15 @@
 class Analyzer {
+    fun getTop10App(list: List<App>):List<String>{
+        var counter = 0
+        val topTen = mutableListOf<String>()
 
+        run breaking@ {
+            list.sortedByDescending { it.installs }.forEach {
+                topTen.add(it.appName)
+                counter++
+                if (counter == 10) return@breaking
+            }
+        }
+        return topTen
+    }
 }
